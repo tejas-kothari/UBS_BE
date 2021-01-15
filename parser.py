f778581d943da71a6c7b57e95baf46ec95669022
Tejas Kothari <tejask@Tejass-MBP.COMFAST>
2021-01-15 15:00:13 +0800

    NaN values

--- parser.py
+++ parser.py
@@ -1,5 +1,11 @@
+import pandas as pd
+
+
 def seriesToDict(s):
     s_dict = {}
     for index in s.index:
-        s_dict[index] = s[index]
+        if pd.isnull(s[index]):
+            s_dict[index] = ""
+        else:
+            s_dict[index] = s[index]
     return s_dict
